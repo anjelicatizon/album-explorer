232b4960b5f83b08d74ba4d866348ae90fa4b9b4
anjelicatizon <anjelicatizon@gmail.com>
2020-09-24 21:16:57 -0400

    prelim styles, header & footer component and axios

--- src/App.js
+++ src/App.js
@@ -5,13 +5,54 @@ import Footer from "./components/Footer.js"
 import "./App.css";
 
 class App extends React.Component {
+  // Constructor - set initial state
+  constructor() {
+    super();
+    this.state = {
+      album: {}
+    };
+  };
+
+  // USING COMPONENT DID MOUNT TO TEST API CALL BUT IT DOES NOT GO IN DID MOUNT FOR THE FINAL VERSION CUZ THERE WILL BE NO ALBUMS APPEARING ON LOAD
+  componentDidMount(){
+    // API Call
+    axios({
+      method: "GET",
+      url: "https://itunes.apple.com/search",
+      dataResponse: "JSON",
+      params: {
+        term: "Fall Out Boy",
+        country: "CA",
+        media: "music",
+        entity: "album"
+      }
+    }).then((res) => {
+        console.log(res.data.results)
+        // albums is the array of the results returned from API
+        const albums = res.data.results
+      });
+  };
+  
+
   render() {
     return (
       <div className="App">
-        {/* Header */}
+        {/* HEADER */}
         <Header />
 
-        {/* Footer */}
+        {/* FORM */}
+        <section>
+          <div className="wrapper">
+            <form>
+              <label htmlFor="search-bar">Type in an artist to discover their entire discography</label>
+              <input type="text" className="search-bar" placeholder="Type in an artist"/>
+              <input type="submit" className="submit" value="Go"/>
+            </form>
+          </div>
+        </section>
+        
+
+        {/* FOOTER */}
         <Footer />
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ const Header = () => {
         <header>
             <div className="wrapper">
                 <h1>Album Explorer</h1>
-                <p>Type in an artist to discover their entire discography</p>
             </div>
         </header>
     );
