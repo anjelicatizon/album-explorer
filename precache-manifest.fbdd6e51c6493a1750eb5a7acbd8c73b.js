4d4279e2ff94188d9b498cb28439582591977cc0
anjelicatizon <anjelicatizon@gmail.com>
2020-09-28 14:22:01 -0400

    Updates

---
+++ precache-manifest.fbdd6e51c6493a1750eb5a7acbd8c73b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdefe31cc92ab258b4815dcabbe9d17",
+    "url": "/album-explorer/index.html"
+  },
+  {
+    "revision": "59c9085b0aea504cf6f6",
+    "url": "/album-explorer/static/css/main.64413c66.chunk.css"
+  },
+  {
+    "revision": "8c24e397a7e7588a48cd",
+    "url": "/album-explorer/static/js/2.71c010d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/album-explorer/static/js/2.71c010d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c9085b0aea504cf6f6",
+    "url": "/album-explorer/static/js/main.2f8f38a0.chunk.js"
+  },
+  {
+    "revision": "9dcee26ee61f15144a86",
+    "url": "/album-explorer/static/js/runtime-main.2198d566.js"
+  }
+]);
