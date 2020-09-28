00119a997ab410caef2fc4e97b022623e376bc86
anjelicatizon <anjelicatizon@gmail.com>
2020-09-28 14:18:44 -0400

    installed Font Awesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
 import axios from "axios";
 import Header from "./components/Header.js"
 import Footer from "./components/Footer.js"
+// Font Awesome - CAN MAKE A COMPONENT LATER
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSort } from '@fortawesome/free-solid-svg-icons'
 import "./App.css";
 
 class App extends React.Component {
@@ -79,6 +83,12 @@ class App extends React.Component {
     })
   }
 
+  //Tracks Sort button
+  handleSort = (album) => {
+    console.log(album)
+    const sortedResults = [... album].sort()
+  }
+
   render() {
     return (
       <div className="App">
@@ -93,35 +103,43 @@ class App extends React.Component {
               <input type="text" id="newAlbum" className="search-bar" onChange={this.handleChange} value={this.state.userInput}/>
               <input type="submit" className="submit" value="Go" onClick={this.handleClick}/>
             </form>
+
+            {/* Sort Button */}
+            <button className="sort" onClick={this.handleSort}>Sort <FontAwesomeIcon icon={faSort} /></button>
           </div>
         </section>
 
         {/* RESULTS/ALBUM SECTION */}
         {/* Telling render method that once you get info on the albums, map through them and display them in an li*/}
-        {/* REFECTOR THIS LATER WITH PROPS & A COMPONENT */}
-          {this.state.albums.map((album) => {
-            console.log(album)
+        {/* REFACTOR THIS LATER WITH PROPS & A COMPONENT */}
+        {this.state.albums.map((album) => {
+          console.log(album)
 
-            const artistName = album.artistName
-            const albumArt = album.artworkUrl100
-            const albumName = album.collectionName
-            const explicitAlert = album.collectionExplicitness
-            const releaseDate = album.releaseDate
-            const id = album.collectionId
+          // Variables for specific pieces of info from the API
+          const artistName = album.artistName
+          const albumArt = album.artworkUrl100
+          const albumName = album.collectionName
+          const explicitAlert = album.collectionExplicitness
+          const releaseDate = album.releaseDate
+          const id = album.collectionId
 
-            return (
+          return (
               <section className="wrapper">
+                
+                {/* Album results */}
                 <ul key={id}>
+                  
                   <li>
-                    <img src={albumArt} alt="FILL IN LATER"/>
+                    <img src={albumArt} alt="Album artwork"/>
                     <h3>{albumName}</h3>
                     <p>{explicitAlert}</p>
                     <p>Released: {releaseDate.slice(0,10)}</p>
                   </li>
                 </ul>
+
               </section>
-            )
-          })};
+          )
+        })};
         
         {/* FOOTER */}
         <Footer />
